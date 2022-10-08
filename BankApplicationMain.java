920e939b70c9f04d87acc43d99506a6921e7a344
namratagheg <102477311+namratagheg@users.noreply.github.com>
2022-10-08 21:32:46 +0530

    Add files via upload

---
+++ BankApplicationMain.java
@@ -0,0 +1,11 @@
+
+public class BankApplicationMain {
+
+	public static void main(String[] args) {
+
+		MyBankAccount ac = new MyBankAccount("NAMRATA GHEGADMAL", "20000");
+		ac.showMenu();	
+
+	}
+
+}
